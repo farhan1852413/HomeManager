9a5b6eb3075e0e5ac04ea82b2c4e8f35ddc3d924
farhan <kotagasti.cs23@bmsce.ac.in>
2026-06-04 22:24:46 +0530

    Fix ChartJS scale registration

--- frontend/src/components/Electricity.js
+++ frontend/src/components/Electricity.js
@@ -1,4 +1,23 @@
-import { Bar } from "react-chartjs-2"
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const Electricity = () => {
     return (
